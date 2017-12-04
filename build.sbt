033140f4a1fda040d89045c985766f1a4f5ae6e5
christopher.walker <christopher.walker@digital.hmrc.gov.uk>
2017-12-04 23:35:08 +0000

    updated headers and refactored config capture

--- build.sbt
+++ build.sbt
@@ -1,26 +1,29 @@
 import com.typesafe.config.ConfigFactory
 import scala.util.{Failure, Success, Try}
 
+val libraryName = "metrics-reporter"
+
 val bTVersion : String = Try(ConfigFactory.load.getString("version")) match {
   case Success(ver) => ver
   case Failure(_)   => "0.1.0"
 }
 
-name          := "metrics-reporter"
-version       := bTVersion
-scalaVersion  := "2.11.11"
-organization  := "com.cjww-dev.libs"
-
-libraryDependencies ++= Seq(
+val dependencies: Seq[ModuleID] = Seq(
   "com.typesafe.play"      % "play_2.11"        % "2.5.16",
   "com.codahale.metrics"   % "metrics-graphite" % "3.0.2",
   "de.threedimensions"    %% "metrics-play"     % "2.5.13"
 )
 
-resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
-resolvers += "breadfan" at "https://dl.bintray.com/breadfan/maven/"
-
-bintrayOrganization                   := Some("cjww-development")
-bintrayReleaseOnPublish in ThisBuild  := true
-bintrayRepository                     := "releases"
-bintrayOmitLicense                    := true
+lazy val library = Project(libraryName, file("."))
+  .settings(
+    version                               :=  bTVersion,
+    scalaVersion                          :=  "2.11.12",
+    organization                          :=  "com.cjww-dev.libs",
+    resolvers                             +=  "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
+    resolvers                             +=  "breadfan" at "https://dl.bintray.com/breadfan/maven/",
+    libraryDependencies                   ++= dependencies,
+    bintrayOrganization                   :=  Some("cjww-development"),
+    bintrayReleaseOnPublish in ThisBuild  :=  true,
+    bintrayRepository                     :=  "releases",
+    bintrayOmitLicense                    :=  true
+  )
--- src/main/scala/com/cjwwdev/metrics/GraphiteReporter.scala
+++ src/main/scala/com/cjwwdev/metrics/GraphiteReporter.scala
@@ -1,18 +1,19 @@
-/*
- * Copyright 2017 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (C) 2016-2017 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.cjwwdev.metrics
 
 import java.net.InetSocketAddress
@@ -21,38 +22,35 @@ import javax.inject.{Inject, Singleton}
 
 import com.codahale.metrics.graphite.{Graphite, GraphiteReporter => GR}
 import com.codahale.metrics.{MetricFilter, SharedMetricRegistries}
+import com.typesafe.config.Config
 import play.api.{Application, Configuration, Logger}
 
 private class MissingMetricsConfigException extends Exception
 
 @Singleton
 class GraphiteReporterImpl @Inject()(app: Application, configuration: Configuration) extends GraphiteReporter {
-  def microserviceMetricsConfig(implicit app: Application): Option[Configuration] = app.configuration.getConfig(s"metrics")
-  private def enabled(app: Application) : Boolean = metricsPluginEnabled(app) && graphitePublisherEnabled(app)
-  private def metricsPluginEnabled(app: Application) : Boolean = microserviceMetricsConfig(app).flatMap(_.getBoolean("metrics.enabled")).getOrElse(false)
-  private def graphitePublisherEnabled(app: Application) : Boolean = microserviceMetricsConfig(app).flatMap(_.getBoolean("graphite.enabled")).getOrElse(false)
-  private def registryName(app: Application) = app.configuration.getString("metrics.name").getOrElse("default")
+  private val metricsConfig: Config    = configuration.underlying.getConfig("metrics")
+  private val metricsEnabled: Boolean  = metricsConfig.getBoolean("enabled")
+  private val graphiteEnabled: Boolean = metricsConfig.getBoolean("graphite.enabled")
+  private val enabled: Boolean         = metricsEnabled && graphiteEnabled
+
+  private val registryName: String     = metricsConfig.getString("name")
+
 
   private def initialiseGraphiteReporter(app: Application): Unit = {
     Logger.info("Initialising graphite reporting module")
 
-    val metricsConfig = microserviceMetricsConfig(app).getOrElse(throw new MissingMetricsConfigException)
+    val graphite = new Graphite(new InetSocketAddress(metricsConfig.getString("graphite.host"), metricsConfig.getInt("graphite.port")))
+    val prefix = metricsConfig.getString("graphite.prefix")
 
-    val graphite = new Graphite(new InetSocketAddress(
-      metricsConfig.getString("graphite.host").getOrElse("graphite"),
-      metricsConfig.getInt("graphite.port").getOrElse(2003)))
-
-    val prefix = metricsConfig.getString("graphite.prefix").getOrElse(s"tax.${app.configuration.getString("appName")}")
-
-    val reporter = GR.forRegistry(
-      SharedMetricRegistries.getOrCreate(registryName(app)))
+    val reporter = GR.forRegistry(SharedMetricRegistries.getOrCreate(registryName))
       .prefixedWith(s"$prefix.${java.net.InetAddress.getLocalHost.getHostName}")
       .convertRatesTo(SECONDS)
       .convertDurationsTo(MILLISECONDS)
       .filter(MetricFilter.ALL)
       .build(graphite)
 
-    reporter.start(metricsConfig.getLong("graphite.interval").getOrElse(10L), SECONDS)
+    reporter.start(metricsConfig.getLong("graphite.interval"), SECONDS)
   }
 
   initialiseGraphiteReporter(app)
